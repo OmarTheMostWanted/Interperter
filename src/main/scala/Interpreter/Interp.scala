736e90ba9bc7f757c13a5a01f7a44a057c749dd0
themostwanted <omarthabettmw@gmail.com>
2020-03-22 04:35:25 +0100

    wasted time with no results

--- src/main/scala/Interpreter/Interp.scala
+++ src/main/scala/Interpreter/Interp.scala
@@ -70,39 +70,95 @@ object Interp {
     }
   }
 
-  def substitute(function: ExprC, params: List[String], args: List[Value]): ExprC = {
+  def substitute(function: ExprC, binds: List[Bind], params: List[String], args: List[Value]): ExprC = {
     function match {
+      case ValC(a) => ValC(a)
       case NumC(n) => NumC(n)
       case TrueC() => TrueC()
       case FalseC() => FalseC();
-      case PlusC(l, r) => PlusC(substitute(l, params, args), substitute(r, params, args))
-      case MultC(l, r) => MultC(substitute(l, params, args), substitute(r, params, args))
-      case IfC(c, t, e) => IfC(substitute(c, params, args), substitute(t, params, args), substitute(e, params, args))
-      case EqNumC(l, r) => EqNumC(substitute(l, params, args), substitute(r, params, args))
-      case LtC(l, r) => LtC(substitute(l, params, args), substitute(r, params, args))
+      case PlusC(l, r) => PlusC(substitute(l, binds, params, args), substitute(r, binds, params, args))
+      case MultC(l, r) => MultC(substitute(l, binds, params, args), substitute(r, binds, params, args))
+      case IfC(c, t, e) => IfC(substitute(c, binds, params, args), substitute(t, binds, params, args), substitute(e, binds, params, args))
+      case EqNumC(l, r) => EqNumC(substitute(l, binds, params, args), substitute(r, binds, params, args))
+      case LtC(l, r) => LtC(substitute(l, binds, params, args), substitute(r, binds, params, args))
       case NilC() => NilC()
-      case ConsC(l, r) => ConsC(substitute(l, params, args), substitute(r, params, args))
-      case HeadC(e) => HeadC(substitute(e, params, args))
-      case TailC(e) => TailC(substitute(e, params, args))
-      case IsNilC(e) => IsNilC(substitute(e, params, args))
-      case IsListC(e) => IsListC(substitute(e, params, args))
+      case ConsC(l, r) => ConsC(substitute(l, binds, params, args), substitute(r, binds, params, args))
+      case HeadC(e) => HeadC(substitute(e, binds, params, args))
+      case TailC(e) => TailC(substitute(e, binds, params, args))
+      case IsNilC(e) => IsNilC(substitute(e, binds, params, args))
+      case IsListC(e) => IsListC(substitute(e, binds, params, args))
       case UndefinedC() => throw CustomInterpException("Undefined function")
+      case AppC(f1, args1) => AppC(substitute(f1, binds, params, args), args1.map(e => substitute(e, binds, params, args)))
 
-      case AppC(f1, args1) => AppC(substitute(f1, params, args), args1.map(e => substitute(e, params, args)))
-
+      //      case IdC(s) => {
+      //        if (binds.contains((s, _))) {
+      //          ValC((binds(binds.lastIndexOf((s, _))).value))
+      //        } else IdC(s)
+      //      }
       case IdC(s) => {
-
         if (params.contains(s)) {
-          ValC(args(params.indexOf(s)))
-        }
-        else IdC(s)
+          ValC(args(params.lastIndexOf(s)))
+        } else IdC(s)
       }
+
+      //      case FdC(params1, body1) => {
+      //        if (params1.equals(params)) {
+      //          FdC(params1, body1)
+      //        } else FdC(params1, substitute(body1, params1, args))
+      //      }
+
       case FdC(params1, body1) => {
-        if (params1.equals(params)) FdC(params1, body1)
-        else FdC(params1, substitute(body1, params1, args))
+        if (params1.equals(params)) {
+          FdC(params1, body1)
+        } else FdC(params1, substitute(body1, binds, params, args))
       }
+
       case _ => throw CustomInterpException("wrong subst type")
     }
+  }
+
+  def createBindList(params: List[String], args: List[Value]): List[Bind] = {
+    params.zip(args).map {
+      case (s, v) => Bind(s, v)
+    }
+  }
+
+  def substitute(function: ExprC, params: List[String], args: List[Value]): ExprC = {
+
+    substitute(function, createBindList(params, args), params, args)
+
+    //    function match {
+    //      case NumC(n) => NumC(n)
+    //      case TrueC() => TrueC()
+    //      case FalseC() => FalseC();
+    //      case PlusC(l, r) => PlusC(substitute(l, params, args), substitute(r, params, args))
+    //      case MultC(l, r) => MultC(substitute(l, params, args), substitute(r, params, args))
+    //      case IfC(c, t, e) => IfC(substitute(c, params, args), substitute(t, params, args), substitute(e, params, args))
+    //      case EqNumC(l, r) => EqNumC(substitute(l, params, args), substitute(r, params, args))
+    //      case LtC(l, r) => LtC(substitute(l, params, args), substitute(r, params, args))
+    //      case NilC() => NilC()
+    //      case ConsC(l, r) => ConsC(substitute(l, params, args), substitute(r, params, args))
+    //      case HeadC(e) => HeadC(substitute(e, params, args))
+    //      case TailC(e) => TailC(substitute(e, params, args))
+    //      case IsNilC(e) => IsNilC(substitute(e, params, args))
+    //      case IsListC(e) => IsListC(substitute(e, params, args))
+    //      case UndefinedC() => throw CustomInterpException("Undefined function")
+    //
+    //      case AppC(f1, args1) => AppC(substitute(f1, params, args), args1.map(e => substitute(e, params, args)))
+    //
+    //      case IdC(s) => {
+    //
+    //        if (params.contains(s)) {
+    //          ValC(args(params.indexOf(s)))
+    //        }
+    //        else IdC(s)
+    //      }
+    //      case FdC(params1, body1) => {
+    //        if (params1.equals(params)) FdC(params1, body1)
+    //        else FdC(params1, substitute(body1, params1, args))
+    //      }
+    //      case _ => throw CustomInterpException("wrong subst type")
+    //    }
   }
 
 
